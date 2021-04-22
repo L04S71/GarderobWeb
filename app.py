3af0cbbf976516722714070271939f04854dcb8d
L04S71 <akimaksim@yandex.ru>
2021-04-22 16:38:15 +0300

    add card actions

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, make_response, request
+from flask import Flask, render_template, make_response, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.sqlite import DATETIME
 
 
 app = Flask(__name__)
@@ -22,6 +23,16 @@ class Clothes(db.Model):
     __tablename__ = 'Foto_Clothes'
     id_foto = db.Column(db.Integer, primary_key=True)
     Foto = db.Column(db.BLOB)
+    Comment = db.Column(db.Text)
+    Data = DATETIME(storage_format="%(day)02d/%(month)02d/%(year)04d")
+    Id_Clothes = db.Column(db.Integer)
+
+class TypeClothes(db.Model):
+    __tablename__ = 'Type_Clothes'
+    ID_Clothes = db.Column(db.Integer, primary_key=True)
+    Name_Clothes = db.Column(db.String(20))
+    Gender = db.Column(db.String(10))
+    ID_Season = db.Column(db.Integer)
 
 class Season(db.Model):
     __tablename__ = 'Season'
@@ -47,6 +58,36 @@ def catalog():
 
     items = CatalogItem.query.filter(CatalogItem.Name_Season.in_(seasons)).filter(CatalogItem.Gender.in_(genders)).all()
     return render_template("catalog.html", items=items)
+
+@app.route('/add', methods=["POST"])
+def add():
+    file = request.files["item-image"]
+    image = file.read()
+
+    clothes = TypeClothes.query.all()
+    row = next((c for c in clothes if c.Name_Clothes == request.form['item-clothes']
+        and c.Gender == request.form['item-gender']
+        and c.ID_Season == int(request.form['item-season'])), None)
+    if not row:
+        row = TypeClothes(
+            Name_Clothes=request.form['item-clothes'],
+            Gender=request.form['item-gender'],
+            ID_Season=int(request.form['item-season']))
+        db.session.add(row)
+        db.session.flush()
+
+    item = Clothes(Data="date('now')", Comment=request.form['item-comment'], Id_Clothes=row.ID_Clothes, Foto=image)
+    db.session.add(item)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/del', methods=["POST"])
+def delete():
+    id = request.form.get("id")
+    item = Clothes.query.get(int(id))
+    db.session.delete(item)
+    db.session.commit()
+    return redirect('/')
 
 @app.route('/')
 def index():
